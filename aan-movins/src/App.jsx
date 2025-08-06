3816e206bffdb73ac4f6d9854320eb785d869b25
netanelchaim <netanelchaim0@gmail.com>
2025-08-06 13:34:22 +0300

    test

--- aan-movins/src/App.jsx
+++ aan-movins/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-   
-    </>
-  )
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
